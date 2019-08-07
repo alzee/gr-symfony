1fd450775a96b8493642e696bd5470112fc4247a
Arc Zee <z@arcz.ee>
2019-08-07 22:25:40 +0800

    nth

--- public/i.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * vim:ft=php et ts=4 sts=4
- * @author z14 <z@arcz.ee>
- * @version
- * @todo
- */
-
-phpinfo();
---
+++ src/Auth.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * vim:ft=php et ts=4 sts=4
+ * @author z14 <z@arcz.ee>
+ * @version
+ * @todo
+ */
+
+namespace App;
+
+class Auth
+{
+    private $session_dir;
+    private $b;
+    private $c;
+    
+    // get sessionid from cookie
+    // if check session ok {
+    //
+    // }
+    // else {
+    // redirect to login
+    // }
+    
+    public function __construct()
+    {
+    }
+
+    public function __destruct()
+    {
+    }
+}
+
